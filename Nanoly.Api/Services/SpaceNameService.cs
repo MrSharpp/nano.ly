7796cd16dd58effb3c4eb034ac113f382285f12c
Amir Alam <sharpprogrammer2018@gmail.com>
2023-10-12 02:12:53 +0530

    add space name service

--- Nanoly.Api/Services/SpaceNameService.cs
+++ Nanoly.Api/Services/SpaceNameService.cs
@@ -13,7 +13,21 @@ public class SpaceNameService
 
     public async Task<List<SpaceName>> getAllSpaceNames(int id)
     {
-        return await _dbContext.SpaceName.FromSql($"SELECT * FROM \"SpaceName\" WHERE \"UserId\" = {id}").ToListAsync();
+        return await _dbContext.User.Where(x => x.Id == id).SelectMany(x => x.spaceNames).ToListAsync();
     }
 
+    public async Task<SpaceName> addSpaceName(string name, string content)
+    {
+        var spaceName = new SpaceName()
+        {
+            name = name,
+            content = content
+        };
+
+        await _dbContext.SpaceName.AddAsync(spaceName);
+
+        await _dbContext.SaveChangesAsync();
+
+        return spaceName;
+    }
 }
