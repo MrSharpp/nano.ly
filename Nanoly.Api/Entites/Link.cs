2fa47de283a1ceb464a572b0039f1682b8a1fce4
Amir Alam <sharpprogrammer2018@gmail.com>
2023-10-17 08:24:53 +0530

    unique constraint on composite columns

--- Nanoly.Api/Entites/Link.cs
+++ Nanoly.Api/Entites/Link.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
 namespace Nanoly.Entities;
 
+[Index(nameof(name), nameof(SpaceNameId), IsUnique = true)]
 public class Link
 {
     public int Id { get; set; }
     public string name { get; set; }
+
+    [ForeignKey("SpaceName")]
+    public int SpaceNameId { get; set; }
+
     public string link { get; set; }
 
 }
---
+++ Nanoly.Api/Migrations/20231017025435_unique_constaint_on_composite_index.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class unique_constaint_on_composite_index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Link_SpaceName_SpaceNameId",
+                table: "Link");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpaceNameId",
+                table: "Link",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Link_name_SpaceNameId",
+                table: "Link",
+                columns: new[] { "name", "SpaceNameId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Link_SpaceName_SpaceNameId",
+                table: "Link",
+                column: "SpaceNameId",
+                principalTable: "SpaceName",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Link_SpaceName_SpaceNameId",
+                table: "Link");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Link_name_SpaceNameId",
+                table: "Link");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SpaceNameId",
+                table: "Link",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Link_SpaceName_SpaceNameId",
+                table: "Link",
+                column: "SpaceNameId",
+                principalTable: "SpaceName",
+                principalColumn: "Id");
+        }
+    }
+}
