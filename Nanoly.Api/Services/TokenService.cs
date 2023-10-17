50059c64f0f8f7f687f1caceb8824d987b60ac98
Amir Alam <sharpprogrammer2018@gmail.com>
2023-10-17 06:37:56 +0530

    refresh token method

--- Nanoly.Api/Services/TokenService.cs
+++ Nanoly.Api/Services/TokenService.cs
@@ -1,5 +1,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using Nanoly.Entities;
@@ -26,5 +27,15 @@ public class TokenService
         var token = new JwtSecurityToken(_config["JwtSettings:Issuer"], _config["JwtSettings:Audience"], claims, null, DateTime.UtcNow.AddHours(1), signinCredentials);
 
         return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    public string RefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
     }
 }
