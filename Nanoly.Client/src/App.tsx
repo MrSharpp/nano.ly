2b70ffed3d50fdb5cff670ade1137984c793d6e7
Amir Alam <sharpprogrammer2018@gmail.com>
2023-10-20 00:04:11 +0530

    login

--- Nanoly.Client/src/App.tsx
+++ Nanoly.Client/src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes } from "react-router-dom";
-
+import { BrowserRouter, Routes } from 'react-router-dom'
+import { Login } from './Features/Authentication/Login'
+import '@mantine/core/styles.css'
 
 function App() {
+    if (true) return <Login />
 
-  return (
-   <BrowserRouter>
-    <Routes>
-      
-    </Routes>
-   </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes></Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App
