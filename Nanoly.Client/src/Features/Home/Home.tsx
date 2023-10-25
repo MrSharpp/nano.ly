02eef58fa152d652418825ac015e5b4e09006427
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-10-25 07:48:55 +0530

    Update Home.tsx

--- Nanoly.Client/src/Features/Home/Home.tsx
+++ Nanoly.Client/src/Features/Home/Home.tsx
@@ -1,26 +1,36 @@
 import { ActionIcon, Container, Flex, Text, TextInput } from '@mantine/core'
+import { IconSearch } from '@tabler/icons-react'
 
 export function Home() {
     return (
         <Container>
-            <Flex mt={'xl'} direction={'column'} gap={'xl'}>
+            <Flex mt={'xl'} direction={'column'} gap={'xl'} align={'center'}>
                 <Text
                     size={'xl'}
                     fw={900}
-                    variant="gradient"
+                    c={'#2C2E33'}
+                    // variant="gradient"
                     mt={'xl'}
-                    gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+                    // gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
                     style={{
                         fontSize: 75,
                         textAlign: 'center',
                     }}
                 >
-                    Find Your Nano Link
+                    Find Your All In One 🚪 To Virtual You
                 </Text>
                 <TextInput
                     size="xl"
                     placeholder="Search a word... eg: mrsharp"
-                    rightSection={<ActionIcon />}
+                    w={400}
+                    rightSection={
+                        <ActionIcon variant="gradient" size={'lg'}>
+                            <IconSearch
+                                size={'xs'}
+                                style={{ width: '70%', height: '70%' }}
+                            />
+                        </ActionIcon>
+                    }
                 />
             </Flex>
         </Container>
