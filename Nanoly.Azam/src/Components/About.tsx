ca751b7e3a93fe5a1e258666536b722cb41e5801
Syed Mohd Azam <smazamamu@gmail.com>
2023-10-31 22:51:40 +0530

    Navigation Done

--- Nanoly.Azam/src/Components/About.tsx
+++ Nanoly.Azam/src/Components/About.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
-  return <div>About Us Page</div>;
+  return (
+    <>
+      <header>
+        <a href="#">Logo</a>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="about">About</Link>
+            </li>
+            <li>
+              <Link to="contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <p>Welcome to About Us Page!</p>
+      </section>
+    </>
+  );
 };
 
 export default About;
--- Nanoly.Azam/src/Components/Home.tsx
+++ Nanoly.Azam/src/Components/Home.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <header>
+        <a href="#">Logo</a>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="about">About</Link>
+            </li>
+            <li>
+              <Link to="contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <p>Welcome to Home Page!</p>
+      </section>
+    </>
+  );
 };
 export default Home;
