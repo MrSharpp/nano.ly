1951865669a4e79112b9e4a30d166f9ee981172d
addy3276 <mynameadil@gmail.com>
2023-12-02 02:53:51 +0530

    Revert "Just commit"
    This reverts commit 214e02b2bb58ca617518e690356f3b117bfe33d2.

--- Nanoly.Adil/src/App.tsx
+++ Nanoly.Adil/src/App.tsx
@@ -1,29 +1,16 @@
 
 import { BrowserRouter ,Routes ,Route } from 'react-router-dom'
-// import Home from './Pages/Home1'
-// import Blogs from './Pages/Homecomponents/Blogs1'
-//import { MantineProvider  } from '@mantine/core';
-import Home from "./Pages/Home"
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import Trial from './Pages/trial';
-
-
-
+import Home from './Pages/Home'
+import Blogs from './Pages/Homecomponents/Blogs'
 function App() {
   return (
-   <MantineProvider>
    <BrowserRouter>
    <Routes>
     <Route path='/' element = {<Home/>} />
-    <Route path='/trial' element = {<Trial/>} />
-    {/* <Route path='/' element = {<Home/>} />
     <Route path="/blog/:id" element={<Blogs/>} />
-    */}
+   
    </Routes>
    </BrowserRouter>
-    </MantineProvider>
-   
     
   )
 }
